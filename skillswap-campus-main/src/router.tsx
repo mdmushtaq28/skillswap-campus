dae78cd281f587e97ada99252f51776c632a2dfe
mdmushtaq28 <mdmushtaq9133@gmail.com>
2026-06-23 09:52:00 +0530

    Update project

--- skillswap-campus-main/src/router.tsx
+++ skillswap-campus-main/src/router.tsx
@@ -1,4 +1,4 @@
-import { createRouter, useRouter } from "@tanstack/react-router";
+import { createRouter as createTanStackRouter, useRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 function DefaultErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
@@ -54,8 +54,8 @@ function DefaultErrorComponent({ error, reset }: { error: Error; reset: () => vo
   );
 }
 
-export const getRouter = () => {
-  const router = createRouter({
+export function createRouter() {
+  const router = createTanStackRouter({
     routeTree,
     context: {},
     scrollRestoration: true,
@@ -64,4 +64,8 @@ export const getRouter = () => {
   });
 
   return router;
+}
+
+export const getRouter = () => {
+  return createRouter();
 };
